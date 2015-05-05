11ca1742cff3c4577b86c901e64c761fff6ba5af
matthew32 <mmartinezmateo93@gmail.com>
2015-05-06 01:23:12 +0200

    1 Version

--- app/src/main/java/com/proyecto/gmwork/proyectoandroid/Gestor/GestorPersistencia.java
+++ app/src/main/java/com/proyecto/gmwork/proyectoandroid/Gestor/GestorPersistencia.java
@@ -14,7 +14,18 @@ public class GestorPersistencia  extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE CLIENTE(" +
+                "ID INT PRIMARY KEY     NOT NULL," +
+                "NIF    CHAR(10)," +
+                "NOMBRE CHAR(50)  ," +
+                "APELLIDOS  CHAR(50)," +
+                "LONGITUD   DOUBLE," +
+                " LATITUD   DOUBLE" +
+                "CALLE TEXT," +
+                "POBLACION TEXT," +
+                "PROXIMAVISITA DATE," +
+                
+                ");");
     }
 
     @Override
